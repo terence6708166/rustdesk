f3919272543fbee19a416f3de62c80178abc6b4c
Terence6708166 <top3@vnc.cc>
2025-07-12 09:27:30 +0800

    圖標換色

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -189,10 +189,10 @@ buildRightPane(BuildContext context) {
              mainAxisAlignment: MainAxisAlignment.center, 
              children: [ 
                Icon( 
-                 // 替換為新圖標
+                 // 替換為左右箭頭圖標
 				 Icons.social_distance,
 				 size: 64, 
-                 color: Colors.grey, 
+                 color: Colors.blue,  //藍色
                ), 
                SizedBox(height: 16), 
                Text( 
